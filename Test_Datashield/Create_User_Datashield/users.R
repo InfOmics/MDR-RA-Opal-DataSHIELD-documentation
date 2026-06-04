2eb5e106ce3dfbc5ca2ce09712139552e0c3786b
SimoneAvesani <id530zug@studenti.univr.it>
2026-06-04 16:56:44 +0200

    Update testing scripts (#43)
    * Update testing scripts
    
    * Update Test-datashield-load-data_as_table.R
    
    * Update users.R

--- Test_Datashield/Create_User_Datashield/users.R
+++ Test_Datashield/Create_User_Datashield/users.R
@@ -13,16 +13,17 @@ url="https://your-domain.com"
 user_name='administrator'
 password= 'administrator'
 
-#connect to server 
+
+#connect to server
 connections <- opal.login(user_name,password, url=url)
 
 
-check_pw_format = function(password, user) 
+check_pw_format = function(password, user)
 {
   pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]{8,}$"
   # Check password validity
   is_valid <- grepl(pattern, password, perl = TRUE) & !is.na(password)
-  
+
   if (is_valid) {
     message(paste("User:", user, "- The password is valid."))
     return(TRUE)
@@ -34,26 +35,29 @@ check_pw_format = function(password, user)
 
 user_name="user-test"
 password= 'P@ssw0rd'
-groups= c('opal-user')
+#groups= c('opal-user')
 
-#check password to create user. 
+#check password to create user.
 #check_pw_format("Passw0rd!", "Alice")  # Valid password (TRUE)
 #check_pw_format("password", "Bob")     # Invalid password (FALSE)
 #check_pw_format("Short1!", "Charlie")  # Invalid (too short)
 #check_pw_format(NA, "Dave")            # Invalid (missing)
 
-check_pw_format(password,user_name) #Valid 
+check_pw_format(password,user_name) #Valid
 
 #if new user create and add permission.
+
 if(!oadmin.user_exists(connections,user_name))
   {
-  
-    oadmin.user_add(connections,name = user_name, password = password, groups = groups)  
+
+    #oadmin.user_add(connections,name = user_name, password = password, groups = groups)
+    oadmin.user_add(connections,name = user_name, password = password)
+
 
     if(oadmin.user_exists(connections,user_name))
     {
       #give permission to use datashield to perform analysis
-      dsadmin.perm_add(connections, subject = groups , type = "group", permission = "use")
+      dsadmin.perm_add(connections, subject = user_name, type = "user", permission = "use")
       # verify permissions
       dsadmin.perm(connections)
 
